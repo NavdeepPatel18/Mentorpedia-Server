8019dd357c582d6d69b772109d2b804a9e1bfcfd
navdeeppatel1852 <navdeeppatel.1852@gmail.com>
2021-03-22 23:24:30 +0530

    create separate api on test(temp.json) data

--- Routes/users.js
+++ Routes/users.js
@@ -1,23 +1,109 @@
 const userRoutes = (app, fs) => {
-
   // variables
-  const dataPath = './Data/data.json';
+  const dataPath = "./Data/temp.json";
 
-  // READ
-  app.get('/users', (req, res) => {
-      fs.readFile(dataPath, 'utf8', (err, data) => {
-          if (err) {
-              throw err;
-          }
+  // READ Faculty name
+  app.get("/faculty-name", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
 
-          var obj = JSON.parse(data);
-          var x = [];
-          for (i = 0; i < obj.length; i++) {
-            x.push(obj[i]['name']);
+      var obj = JSON.parse(data);
+      var x = { faculty_name: []};
+      for (i = 0; i < obj.length; i++) {
+        x.faculty_name.push(obj[i]["name"]);
+      }
+      res.send(x);
+      // console.log(obj.length);
+    });
+  });
+
+  
+  // READ collage name
+  app.get("/collage", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      var obj = JSON.parse(data);
+      var x = { college: []};
+      for (i = 0; i < obj.length; i++) {
+        if (x.college.includes(obj[i]["college"]) === false)
+          x.college.push(obj[i]["college"]);
+      }
+      res.send(x);
+      // console.log(obj.length);
+    });
+  });
+
+
+  // READ department name
+  app.get("/dept", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      var obj = JSON.parse(data);
+      var x = { dept: [] };
+      for (i = 0; i < obj.length; i++) {
+        if (x.dept.includes(obj[i]["dept"]) === false)
+          x.dept.push(obj[i]["dept"]);
+      }
+      res.send(x);
+      // console.log(obj.length);
+    });
+  });
+
+
+  // READ  research areas
+  app.get("/research-areas", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      var obj = JSON.parse(data);
+      var x = { research_areas: [] };
+      for (i = 0; i < obj.length; i++) {
+
+        if(obj[i]["research_areas"].constructor === Array){
+          for(j=0;j<obj[i]["research_areas"].length;j++){
+            x.research_areas.push(obj[i]["research_areas"][j]);
           }
-          res.send(x);
-          // console.log(obj.length);
-      });
+        }
+        else
+          x.research_areas.push(obj[i]["research_areas"]);
+      }
+      res.send(x);
+      // console.log(obj.length);
+    });
+  });
+
+  // READ  all data of all collage
+  app.get("/iits", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      var obj = JSON.parse(data);
+      var x = { faculty_name: [], college: [], dept: [], research_areas: [] };
+      for (i = 0; i < obj.length; i++) {
+        x.faculty_name.push(obj[i]["name"]);
+        if (x.college.includes(obj[i]["college"]) === false)
+          x.college.push(obj[i]["college"]);
+
+        if (x.dept.includes(obj[i]["dept"]) === false)
+          x.dept.push(obj[i]["dept"]);
+
+        x.research_areas.push(obj[i]["research_areas"]);
+      }
+      res.send(x);
+      // console.log(obj.length);
+    });
   });
 };
 
--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 // load up the express framework and body-parser helper
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // create an instance of express to serve our end points
 const app = express();
@@ -11,6 +12,29 @@ const fs = require('fs');
 
 // configure our express instance with some body-parser settings
 // including handling JSON data
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
